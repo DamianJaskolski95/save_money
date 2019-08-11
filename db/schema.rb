57e4b19abb903282c8c9102c84b8e6fb89761681
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-11 13:28:27 +0200

    categories migration - add created by

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_11_103311) do
+ActiveRecord::Schema.define(version: 2019_08_11_112631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_08_11_103311) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "created_by"
   end
 
   create_table "expenses", force: :cascade do |t|
