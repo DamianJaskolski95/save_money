ded7510dd863218876bf92f7b71d8f476bc3dc1e
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-11 12:34:37 +0200

    user base model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_125355) do
+ActiveRecord::Schema.define(version: 2019_08_11_103311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2019_08_10_125355) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_expenses_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "expenses", "categories"
