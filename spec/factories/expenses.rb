3c1310e6b1c2a8e14c19fc45f02eb0763dd04d4f
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-11 11:57:29 +0200

    Factories for categories and expenses

---
+++ spec/factories/expenses.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :expense do
+    month { Faker::Number.within(range: 1..12) }
+    planned_value { Faker::Number.decimal(r_digits: 2)}
+    value { Faker::Number.decimal(r_digits: 2) }
+    category_id { nil }
+  end
+end
