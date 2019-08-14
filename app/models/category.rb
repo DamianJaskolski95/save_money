d5f7eaea06498a887f54e0f112284ea0bd7e5383
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-14 12:50:00 +0200

    adds swagger model schema

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,35 @@
 class Category < ApplicationRecord
+  include Swagger::Blocks
+
+  swagger_schema :Category do
+    key :required, [:id, :name, :created_by]
+    property :id do
+      key :type, :integer
+    end
+    property :name do
+      key :type, :string
+    end
+    property :created_by do
+      key :type, :integer
+    end
+    property :created_at do
+      key :type, :string
+      key :format, :'date-time'
+    end
+    property :updated_at do
+      key :type, :string
+      key :format, :'date-time'
+    end
+    property :expenses do
+      key :type, :array
+      items do
+        property :id do
+          key :type, :integer
+        end
+      end
+    end
+  end
+  
   has_many :expenses, dependent: :destroy
 
   validates_presence_of :name, :created_by
