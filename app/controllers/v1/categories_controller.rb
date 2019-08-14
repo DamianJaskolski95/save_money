51f00113ada6aba583eb54718befc7fed5758859
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-14 12:47:38 +0200

    adds swagger for categories contrloller

--- app/controllers/v1/categories_controller.rb
+++ app/controllers/v1/categories_controller.rb
@@ -1,6 +1,51 @@
 module V1
   class CategoriesController < ApplicationController
+    include Swagger::Blocks
+
     before_action :set_category, only: [:show, :update, :destroy]
+
+    swagger_path "/categories" do
+      operation :get do
+        key :summary, "Categories Panel"
+        key :description, "Returns categories from the system that the user \
+                      has access to, 25 per page.\nCan get all with parameter \
+                      change."
+        key :tags, [
+          'categories'
+        ]
+        parameter do
+          key :name, :get_all
+          key :in, :query
+          key :required, false
+          key :type, :bool
+        end
+        parameter do
+          key :name, :page
+          key :in, :query
+          key :required, false
+          key :type, :string
+          key :collectionFormat, :csv
+        end
+        response 200 do
+          key :description, 'categories response'
+          schema do
+            key :type, :array
+            items do
+              key :'$ref', :Category
+            end
+          end
+        end
+        response :default do
+          key :description, 'unexpected error'
+          schema do
+            key :'$ref', :ErrorModel
+          end
+        end
+        security do
+          key :api_key, []
+        end
+      end
+    end
 
     def index
       @categories = current_user.categories.paginate(page: params[:page], per_page:25)
