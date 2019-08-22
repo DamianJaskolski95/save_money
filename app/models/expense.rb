28f1e97b1d9a6e4caa78b0b93190b66a99d40cea
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-22 16:00:33 +0200

    additionally validates date format with regex

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -26,5 +26,7 @@ class Expense < ApplicationRecord
     end
   end
 
+  DATE_FORMAT = /\d{4}-\d{1,2}-\d{1,2}/
+  validates :expense_day, format: { with: DATE_FORMAT, message: 'wrong format, YYYY-M(M)-D(D)'}
   validates :planned_value, :value, :numericality => { :greater_than_or_equal_to => 0 }
 end
