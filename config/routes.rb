8253a839187412cf83ea62cb17abc8a1471279d9
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-29 11:25:45 +0200

    adds routes to balaces /users/{id}/balances

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :users do
+      resources :balances
+    end
     resources :categories do
       resources :expenses
     end
