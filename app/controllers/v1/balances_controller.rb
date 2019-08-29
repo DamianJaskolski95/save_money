193316319b14a6614fd40db294d32dac803fbeb7
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2019-08-29 11:26:28 +0200

    adds balances controller, basic crud

---
+++ app/controllers/v1/balances_controller.rb
@@ -0,0 +1,61 @@
+module V1
+  class BalancesController < ApplicationController
+    before_action :set_balance, only: [:show, :update, :destroy]
+
+    def index
+      if params[:get_all] == "true"
+        @balances = current_user.balances
+        json_response(@balances)
+      else
+        @balances = current_user.balances.paginate(page: params[:page], per_page:25)
+        json_response(@balances)
+      end
+    end
+
+    def create
+      @balance = current_user.balances.create!(balance_params)
+      json_response(@balance, :created)
+    end
+
+    def show
+      if current_user_resource?(@balance)
+        json_response(@balance)
+      else
+        json_response(message: Message.unauthorized)
+      end
+    end
+
+    def update
+      if current_user_resource?(@balance)
+        if @balance.update(category_params)
+          json_response(@balance)
+        else
+          json_response(message: Message.unique_value_used)
+        end
+      else
+        json_response(message: Message.unauthorized)
+      end
+    end
+
+    def destroy
+      if current_user_resource?(@balance)
+        if @balance.destroy
+          json_response(message: Message.value_deleted)
+        else
+          json_response(message: Message.value_not_deleted)
+        end
+      else
+        json_response(message: Message.unauthorized)
+      end
+    end
+
+    private
+    def balance_params
+      params.permit(:id, :income, :planned_savings, :savings, :month, :get_all)
+    end
+
+    def set_balance
+      @balance = Balance.find(params[:id])
+    end
+  end
+end
